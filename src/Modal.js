45e8dbfb515d60527badb03f3901b9320d8291ab
Juan Mario Rodriguez <oasistech@Juans-MacBook-Pro.local>
2022-01-11 23:42:34 -0600

    added the finishing touched to the succed form submission component and button styling

--- src/Modal.js
+++ src/Modal.js
@@ -4,8 +4,10 @@ import PropTypes from "prop-types";
 const Modal = ({ modal, setModal }) => {
     return (
         <div className={modal ? "modal-container" : "remove-modal"}>
-            <p>Your form has been submitted</p>
-            <button onClick={() => setModal(false)}>close</button>
+            <div className="message-container">
+                <p>Thank you, form Submitted !</p>
+                <button onClick={() => setModal(false)}>close</button>
+            </div>
         </div>
     );
 };
